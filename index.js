5090873a189268e04ed46b82621b90f95e1d6ded
Shelli Martinez <shelli.mrtnz@gmail.com>
2023-11-19 11:52:28 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+/* 
+Let's say we are hard at work in the battery factory. 
+We've assembled several batches of batteries today. 
+Let's count how many assembled batteries we ended up with.
+
+Create a new variable called totalBatteries, which holds the sum of all of the battery amounts in the batteryBatches array. 
+(Note that the batteryBatches variable has been provided for you in index.js.) Naturally, you should use reduce() for this!
+*/
+
+const totalBatteries = batteryBatches.reduce(function batteryCounter(accumulator, batteries) {
+    return accumulator + batteries;
+  }, 0);
+  
